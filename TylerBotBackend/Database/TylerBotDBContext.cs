61e698a4adcf3985280ec0201df32785a1096fcc
Tyler Siders <78033735+TylerJSiders@users.noreply.github.com>
2022-01-16 11:28:30 -0500

    DbcontextSetup

---
+++ TylerBotBackend/Database/TylerBotDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TylerBotBackend.Database
+{
+    public class TylerBotDBContext : DbContext
+    {
+        public TylerBotDBContext(DbContextOptions<TylerBotDBContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AppDb");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
--- TylerBotBackend/Program.cs
+++ TylerBotBackend/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TylerBotBackend.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<TylerBotDBContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
